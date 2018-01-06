6cfbc122d86645edaa4ccbee97de5463bafdf72c
Eric J. Duran <eric.duran7@gmail.com>
2018-01-06 15:49:37 -0500

    Adding top down, dp, dp + mem fib problem -- need article links

--- fib.js
+++ fib.js
@@ -1,3 +1,34 @@
+// I struggle the most with bottom up technique, DP and MEM are easier for me.
+// There's a great 3 part series on migrating a DP solution to bottom up or
+// top down but I can't find it right now.
+
+// Fibonacci with a bottom up approach.
+function fib(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+  let prevPrev = 0;
+  let prev = 1;
+  let cur = 0;
+  for (let i =1; i < n; i++) {
+    cur = prev + prevPrev;
+    prevPrev = prev;
+    prev = cur;
+  }
+
+  return cur;
+}
+
+// Fibonacci with a Dynamic Programming approach.
+function fibRecursive(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  return fibRecursive(n - 1) + fibRecursive(n - 2);
+}
+
+// Fibonacci with a Dynamic Programming + memoization approach.
 function fibMem(n, mem = []) {
   if (n === 0 || n === 1) {
     return n;
@@ -10,27 +41,6 @@ function fibMem(n, mem = []) {
   return mem[n];
 }
 
-
-function fib(n) {
-  if (n === 0 || n ===1 ) {
-    return n;
-  }
-
-  let prevPrev = 0;
-  let prev = 1;
-  let cur = 0;
-
-  for (let i = 1; i < n; i++) {
-    cur = prev + prevPrev;
-    prevPrev = prev;
-    prev = cur;
-  }
-
-  return cur;
-}
-
-console.log(fib(0), '0');
-console.log(fib(1), '1');
-console.log(fib(2), '2');
-console.log(fib(3), '3');
-console.log(fib(4), '4');
+console.log(fib(8) === 21);
+console.log(fibRecursive(8) === 21);
+console.log(fibMem(8) === 21);
